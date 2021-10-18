440c171541d8046237acbdc7262d838b7353f3a5
Ben McGarvey <b.mcgarvey.bm@gmail.com>
2021-10-18 20:49:22 +1100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/index.css';
+import Root from './Root';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
